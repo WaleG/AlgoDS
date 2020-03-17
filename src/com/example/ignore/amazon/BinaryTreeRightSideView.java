c90be8e2c22757a5ecce3ddc72956c53175e8cb8
Valentin Molyakov <valentin.molyakov@gmail.com>
2020-03-17 18:45:33 +0200

    BinaryTreeRightSideView problem

---
+++ src/com/example/ignore/amazon/BinaryTreeRightSideView.java
@@ -0,0 +1,46 @@
+package com.example.ignore.amazon;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeRightSideView {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) return result;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.remove();
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+                if (i == size - 1) {
+                    result.add(curr.val);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
