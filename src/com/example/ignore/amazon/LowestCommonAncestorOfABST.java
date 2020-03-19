dd78108b3890b019cbdb57bf18bae193286cefdd
Valentin Molyakov <valentin.molyakov@gmail.com>
2020-03-19 15:06:12 +0200

    LowestCommonAncestorOfABST problem

---
+++ src/com/example/ignore/amazon/LowestCommonAncestorOfABST.java
@@ -0,0 +1,21 @@
+package com.example.ignore.amazon;
+
+public class LowestCommonAncestorOfABST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p == q) return p;
+        if (root == p || root == q) return root;
+        else if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        else if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        else return root;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
