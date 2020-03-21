4b0d0ff51cb6417c9857008d65954b79a476b65d
Valentin Molyakov <valentin.molyakov@gmail.com>
2020-03-21 20:02:43 +0200

    LargestRectangleInHistogram problem

---
+++ src/com/example/arrays/LargestRectangleInHistogram.java
@@ -0,0 +1,31 @@
+package com.example.arrays;
+
+import java.util.ArrayDeque;
+import java.util.Stack;
+
+public class LargestRectangleInHistogram {
+    public static void main(String[] args) {
+        int[] heights = {2,1,5,6,2,3};
+        int maxArea = new LargestRectangleInHistogram().largestRectangleArea(heights);
+        System.out.println(maxArea);
+    }
+
+    // Stack-based O(n) solution
+    public int largestRectangleArea(int[] heights) {
+        if (heights == null || heights.length == 0) {
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        for (int i = 0; i <= heights.length; i++) {
+            int currH = (i == heights.length) ? -1 : heights[i];
+            while (!stack.isEmpty() && heights[stack.peek()] >= currH) {
+                int h = heights[stack.pop()];
+                int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, h * w);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+}
