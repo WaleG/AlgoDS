3ec485f7ea641becedcc8ada32e7946846bb8721
Valentin Molyakov <valentin.molyakov@gmail.com>
2020-03-23 16:13:30 +0200

    DiameterOfABinaryTree problem

---
+++ src/com/example/ignore/amazon/DiameterOfABinaryTree.java
@@ -0,0 +1,32 @@
+package com.example.ignore.amazon;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DiameterOfABinaryTree {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] maxHolder = {0};
+        maxDepth(root, maxHolder);
+        return maxHolder[0];
+    }
+
+    private int maxDepth(TreeNode root, int[] maxHolder) {
+        if (root == null) return 0;
+        int left = maxDepth(root, maxHolder);
+        int right = maxDepth(root, maxHolder);
+
+        maxHolder[0] = Math.max(maxHolder[0], left + right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
