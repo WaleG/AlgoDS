776095fedef3e75c987c9e8d8cd382baebc51c78
Valentin Molyakov <valentin.molyakov@gmail.com>
2020-03-28 22:49:19 +0200

    SplitArrayLargestSum problem

--- src/com/example/ignore/amazon/SplitArrayLargestSum.java
+++ src/com/example/ignore/amazon/SplitArrayLargestSum.java
@@ -26,36 +26,73 @@ The best way is to split it into [7,2,5] and [10,8],
 where the largest sum among the two subarrays is only 18.
      */
     public static void main(String[] args) {
-        int[] nums = {7,2,5,10,8};
+        int[] nums = {7, 2, 5, 10, 8};
 //        int[] nums = {1, 2147483647};
         int result = new SplitArrayLargestSum().splitArray(nums, 2);
         System.out.println(result);
     }
 
     public int splitArray(int[] nums, int m) {
-        int[][] memo = new int[nums.length][m + 1];
-        int[] sum = new int[nums.length];
-        sum[0] = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            sum[i] = sum[i - 1] + nums[i];
+        int max = 0;
+        long sum = 0;
+
+        //find sum of all elements and max element
+        for (int num : nums) {
+            max = Math.max(num, max);
+            sum += num;
         }
-        return helper(nums, nums.length - 1, m, sum, memo);
+
+        if (m == 1) return (int) sum;
+
+        //do binary search
+        long l = max, r = sum;
+        while (l <= r) {
+            long mid = l + (r - l) / 2;
+            if (isValid(mid, nums, m)) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int) l;
     }
 
-    private int helper(int[] nums, int start, int m, int[] sums, int[][] memo) {
-        if (m == 1) {
-            return sums[start];
+    private boolean isValid(long target, int[] nums, int m) {
+        int count = 1;
+        long total = 0;
+        for (int num : nums) {
+            total += num;
+            if (total > target) {
+                count++;
+                total = num;
+                if (count > m) return false;
+            }
         }
-        if (memo[start][m] > 0) {
-            return memo[start][m];
-        }
-        int min = Integer.MAX_VALUE;
-        int sum = 0;
-        for (int i = start; i >= 1; i--) {
-            sum += nums[i];
-            min = Math.min(Math.max(sum, helper(nums, i - 1, m - 1, sums, memo)), min);
-        }
-        memo[start][m] = min;
-        return memo[start][m];
+        return true;
     }
+
+//    //top-down
+//    public int splitArray(int[] nums, int m) {
+//        int[][] memo = new int[nums.length][m + 1];
+//        memo[0][1] = nums[0];
+//        for (int i = 1; i < memo.length; i++) {
+//            memo[i][0] = Integer.MAX_VALUE;
+//            memo[i][1] = memo[i - 1][1] + nums[i];
+//        }
+//        return helper(nums, nums.length - 1, m, memo);
+//    }
+//
+//    private int helper(int[] nums, int start, int m, int[][] memo) {
+//        if (memo[start][m] > 0) {
+//            return memo[start][m];
+//        }
+//        int min = Integer.MAX_VALUE;
+//        int sum = 0;
+//        for (int i = start; i >= 1; i--) {
+//            sum += nums[i];
+//            min = Math.min(Math.max(sum, helper(nums, i - 1, m - 1, memo)), min);
+//        }
+//        memo[start][m] = min;
+//        return memo[start][m];
+//    }
 }
